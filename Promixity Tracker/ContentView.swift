1f23377d2d0359b32ed599748f9964c428367992
Jeffrey Abraham <Jeffreyabraham.sep+github.com>
2024-04-02 11:47:03 -0700

    Initial Commit

---
+++ Promixity Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Promixity Tracker
+//
+//  Created by user238598 on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Promixity Tracker/Promixity_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Promixity_TrackerApp.swift
+//  Promixity Tracker
+//
+//  Created by user238598 on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Promixity_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
